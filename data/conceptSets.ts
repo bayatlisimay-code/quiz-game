0c63f19b0a289086e18dcc4e7ccc733a7db32f85
bayatlisimay-code <bayatlisimay@gmail.com>
2026-05-04 11:12:24 +0400

    trying to fix simulator error

--- data/conceptSets.ts
+++ data/conceptSets.ts
@@ -10,6 +10,55 @@ import concepts_art_painting_l2_p3_concepts from "./concepts/art/painting/l2/p3/
 import concepts_art_painting_l2_p4_concepts from "./concepts/art/painting/l2/p4/concepts";
 import concepts_art_painting_l2_p5_concepts from "./concepts/art/painting/l2/p5/concepts";
 
+import concepts_art_painting_l3_p3_concepts from "./concepts/art/painting/l2/p3/concepts";
+import concepts_art_painting_l3_p1_concepts from "./concepts/art/painting/l3/p1/concepts";
+import concepts_art_painting_l3_p2_concepts from "./concepts/art/painting/l3/p2/concepts";
+import concepts_art_painting_l3_p4_concepts from "./concepts/art/painting/l3/p4/concepts";
+import concepts_art_painting_l3_p5_concepts from "./concepts/art/painting/l3/p5/concepts";
+
+import concepts_art_painting_l4_p1_concepts from "./concepts/art/painting/l4/p1/concepts";
+import concepts_art_painting_l4_p2_concepts from "./concepts/art/painting/l4/p2/concepts";
+import concepts_art_painting_l4_p3_concepts from "./concepts/art/painting/l4/p3/concepts";
+import concepts_art_painting_l4_p4_concepts from "./concepts/art/painting/l4/p4/concepts";
+import concepts_art_painting_l4_p5_concepts from "./concepts/art/painting/l4/p5/concepts";
+
+import concepts_art_painting_l5_p1_concepts from "./concepts/art/painting/l5/p1/concepts";
+import concepts_art_painting_l5_p2_concepts from "./concepts/art/painting/l5/p2/concepts";
+import concepts_art_painting_l5_p3_concepts from "./concepts/art/painting/l5/p3/concepts";
+import concepts_art_painting_l5_p4_concepts from "./concepts/art/painting/l5/p4/concepts";
+import concepts_art_painting_l5_p5_concepts from "./concepts/art/painting/l5/p5/concepts";
+
+import concepts_art_painting_l6_p1_concepts from "./concepts/art/painting/l6/p1/concepts";
+import concepts_art_painting_l6_p2_concepts from "./concepts/art/painting/l6/p2/concepts";
+import concepts_art_painting_l6_p3_concepts from "./concepts/art/painting/l6/p3/concepts";
+import concepts_art_painting_l6_p4_concepts from "./concepts/art/painting/l6/p4/concepts";
+import concepts_art_painting_l6_p5_concepts from "./concepts/art/painting/l6/p5/concepts";
+
+import concepts_art_painting_l7_p1_concepts from "./concepts/art/painting/l7/p1/concepts";
+import concepts_art_painting_l7_p2_concepts from "./concepts/art/painting/l7/p2/concepts";
+import concepts_art_painting_l7_p3_concepts from "./concepts/art/painting/l7/p3/concepts";
+import concepts_art_painting_l7_p4_concepts from "./concepts/art/painting/l7/p4/concepts";
+import concepts_art_painting_l7_p5_concepts from "./concepts/art/painting/l7/p5/concepts";
+
+import concepts_art_painting_l8_p1_concepts from "./concepts/art/painting/l8/p1/concepts";
+import concepts_art_painting_l8_p2_concepts from "./concepts/art/painting/l8/p2/concepts";
+import concepts_art_painting_l8_p3_concepts from "./concepts/art/painting/l8/p3/concepts";
+import concepts_art_painting_l8_p4_concepts from "./concepts/art/painting/l8/p4/concepts";
+import concepts_art_painting_l8_p5_concepts from "./concepts/art/painting/l8/p5/concepts";
+
+import concepts_art_painting_l9_p1_concepts from "./concepts/art/painting/l9/p1/concepts";
+import concepts_art_painting_l9_p2_concepts from "./concepts/art/painting/l9/p2/concepts";
+import concepts_art_painting_l9_p3_concepts from "./concepts/art/painting/l9/p3/concepts";
+import concepts_art_painting_l9_p4_concepts from "./concepts/art/painting/l9/p4/concepts";
+import concepts_art_painting_l9_p5_concepts from "./concepts/art/painting/l9/p5/concepts";
+
+import concepts_art_painting_l10_p1_concepts from "./concepts/art/painting/l10/p1/concepts";
+import concepts_art_painting_l10_p2_concepts from "./concepts/art/painting/l10/p2/concepts";
+import concepts_art_painting_l10_p3_concepts from "./concepts/art/painting/l10/p3/concepts";
+import concepts_art_painting_l10_p4_concepts from "./concepts/art/painting/l10/p4/concepts";
+import concepts_art_painting_l10_p5_concepts from "./concepts/art/painting/l10/p5/concepts";
+
+
 export const CONCEPT_SETS: Record<string, { id: string; concepts: any[] }> = {
   art_painting_l1_p1: concepts_art_painting_l1_p1_concepts,
   art_painting_l1_p2: concepts_art_painting_l1_p2_concepts,
@@ -22,4 +71,53 @@ export const CONCEPT_SETS: Record<string, { id: string; concepts: any[] }> = {
   art_painting_l2_p3: concepts_art_painting_l2_p3_concepts,
   art_painting_l2_p4: concepts_art_painting_l2_p4_concepts,
   art_painting_l2_p5: concepts_art_painting_l2_p5_concepts,
+
+  art_painting_l3_p1: concepts_art_painting_l3_p1_concepts,
+  art_painting_l3_p2: concepts_art_painting_l3_p2_concepts,
+  art_painting_l3_p3: concepts_art_painting_l3_p3_concepts,
+  art_painting_l3_p4: concepts_art_painting_l3_p4_concepts,
+  art_painting_l3_p5: concepts_art_painting_l3_p5_concepts,
+
+  art_painting_l4_p1: concepts_art_painting_l4_p1_concepts,
+  art_painting_l4_p2: concepts_art_painting_l4_p2_concepts,
+  art_painting_l4_p3: concepts_art_painting_l4_p3_concepts,
+  art_painting_l4_p4: concepts_art_painting_l4_p4_concepts,
+  art_painting_l4_p5: concepts_art_painting_l4_p5_concepts,
+
+  art_painting_l5_p1: concepts_art_painting_l5_p1_concepts,
+  art_painting_l5_p2: concepts_art_painting_l5_p2_concepts,
+  art_painting_l5_p3: concepts_art_painting_l5_p3_concepts,
+  art_painting_l5_p4: concepts_art_painting_l5_p4_concepts,
+  art_painting_l5_p5: concepts_art_painting_l5_p5_concepts,
+
+  art_painting_l6_p1: concepts_art_painting_l6_p1_concepts,
+  art_painting_l6_p2: concepts_art_painting_l6_p2_concepts,
+  art_painting_l6_p3: concepts_art_painting_l6_p3_concepts,
+  art_painting_l6_p4: concepts_art_painting_l6_p4_concepts,
+  art_painting_l6_p5: concepts_art_painting_l6_p5_concepts,
+
+  art_painting_l7_p1: concepts_art_painting_l7_p1_concepts,
+  art_painting_l7_p2: concepts_art_painting_l7_p2_concepts,
+  art_painting_l7_p3: concepts_art_painting_l7_p3_concepts,
+  art_painting_l7_p4: concepts_art_painting_l7_p4_concepts,
+  art_painting_l7_p5: concepts_art_painting_l7_p5_concepts,
+
+  art_painting_l8_p1: concepts_art_painting_l8_p1_concepts,
+  art_painting_l8_p2: concepts_art_painting_l8_p2_concepts,
+  art_painting_l8_p3: concepts_art_painting_l8_p3_concepts,
+  art_painting_l8_p4: concepts_art_painting_l8_p4_concepts,
+  art_painting_l8_p5: concepts_art_painting_l8_p5_concepts,
+
+  art_painting_l9_p1: concepts_art_painting_l9_p1_concepts,
+  art_painting_l9_p2: concepts_art_painting_l9_p2_concepts,
+  art_painting_l9_p3: concepts_art_painting_l9_p3_concepts,
+  art_painting_l9_p4: concepts_art_painting_l9_p4_concepts,
+  art_painting_l9_p5: concepts_art_painting_l9_p5_concepts,
+
+  art_painting_l10_p1: concepts_art_painting_l10_p1_concepts,
+  art_painting_l10_p2: concepts_art_painting_l10_p2_concepts,
+  art_painting_l10_p3: concepts_art_painting_l10_p3_concepts,
+  art_painting_l10_p4: concepts_art_painting_l10_p4_concepts,
+  art_painting_l10_p5: concepts_art_painting_l10_p5_concepts,
+ 
 };
