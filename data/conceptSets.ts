f276fcb7487fa60338ed7f67f5a9f5f312c3dbfe
bayatlisimay-code <bayatlisimay@gmail.com>
2026-05-05 18:23:39 +0400

    new concepts added

--- data/conceptSets.ts
+++ data/conceptSets.ts
@@ -58,6 +58,68 @@ import concepts_art_painting_l10_p3_concepts from "./concepts/art/painting/l10/p
 import concepts_art_painting_l10_p4_concepts from "./concepts/art/painting/l10/p4/concepts";
 import concepts_art_painting_l10_p5_concepts from "./concepts/art/painting/l10/p5/concepts";
 
+import concepts_art_literature_l1_p1_concepts from "./concepts/art/literature/l1/p1/concepts";
+import concepts_art_literature_l1_p2_concepts from "./concepts/art/literature/l1/p2/concepts";
+import concepts_art_literature_l1_p3_concepts from "./concepts/art/literature/l1/p3/concepts";
+import concepts_art_literature_l1_p4_concepts from "./concepts/art/literature/l1/p4/concepts";
+import concepts_art_literature_l1_p5_concepts from "./concepts/art/literature/l1/p5/concepts";
+
+import concepts_art_literature_l2_p1_concepts from "./concepts/art/literature/l2/p1/concepts";
+import concepts_art_literature_l2_p2_concepts from "./concepts/art/literature/l2/p2/concepts";
+import concepts_art_literature_l2_p3_concepts from "./concepts/art/literature/l2/p3/concepts";
+import concepts_art_literature_l2_p4_concepts from "./concepts/art/literature/l2/p4/concepts";
+import concepts_art_literature_l2_p5_concepts from "./concepts/art/literature/l2/p5/concepts";
+
+import concepts_art_literature_l3_p1_concepts from "./concepts/art/literature/l3/p1/concepts";
+import concepts_art_literature_l3_p2_concepts from "./concepts/art/literature/l3/p2/concepts";
+import concepts_art_literature_l3_p3_concepts from "./concepts/art/literature/l3/p3/concepts";
+import concepts_art_literature_l3_p4_concepts from "./concepts/art/literature/l3/p4/concepts";
+import concepts_art_literature_l3_p5_concepts from "./concepts/art/literature/l3/p5/concepts";
+
+import concepts_art_literature_l4_p1_concepts from "./concepts/art/literature/l4/p1/concepts";
+import concepts_art_literature_l4_p2_concepts from "./concepts/art/literature/l4/p2/concepts";
+import concepts_art_literature_l4_p3_concepts from "./concepts/art/literature/l4/p3/concepts";
+import concepts_art_literature_l4_p4_concepts from "./concepts/art/literature/l4/p4/concepts";
+import concepts_art_literature_l4_p5_concepts from "./concepts/art/literature/l4/p5/concepts";
+
+import concepts_art_literature_l5_p1_concepts from "./concepts/art/literature/l6/p1/concepts";
+import concepts_art_literature_l5_p2_concepts from "./concepts/art/literature/l6/p2/concepts";
+import concepts_art_literature_l5_p3_concepts from "./concepts/art/literature/l6/p3/concepts";
+import concepts_art_literature_l5_p4_concepts from "./concepts/art/literature/l6/p4/concepts";
+import concepts_art_literature_l5_p5_concepts from "./concepts/art/literature/l6/p5/concepts";
+
+import concepts_art_literature_l6_p1_concepts from "./concepts/art/literature/l6/p1/concepts";
+import concepts_art_literature_l6_p2_concepts from "./concepts/art/literature/l6/p2/concepts";
+import concepts_art_literature_l6_p3_concepts from "./concepts/art/literature/l6/p3/concepts";
+import concepts_art_literature_l6_p4_concepts from "./concepts/art/literature/l6/p4/concepts";
+import concepts_art_literature_l6_p5_concepts from "./concepts/art/literature/l6/p5/concepts";
+
+import concepts_art_literature_l7_p1_concepts from "./concepts/art/literature/l7/p1/concepts";
+import concepts_art_literature_l7_p2_concepts from "./concepts/art/literature/l7/p2/concepts";
+import concepts_art_literature_l7_p3_concepts from "./concepts/art/literature/l7/p3/concepts";
+import concepts_art_literature_l7_p4_concepts from "./concepts/art/literature/l7/p4/concepts";
+import concepts_art_literature_l7_p5_concepts from "./concepts/art/literature/l7/p5/concepts";
+
+import concepts_art_literature_l8_p1_concepts from "./concepts/art/literature/l8/p1/concepts";
+import concepts_art_literature_l8_p2_concepts from "./concepts/art/literature/l8/p2/concepts";
+import concepts_art_literature_l8_p3_concepts from "./concepts/art/literature/l8/p3/concepts";
+import concepts_art_literature_l8_p4_concepts from "./concepts/art/literature/l8/p4/concepts";
+import concepts_art_literature_l8_p5_concepts from "./concepts/art/literature/l8/p5/concepts";
+
+import concepts_art_literature_l9_p1_concepts from "./concepts/art/literature/l9/p1/concepts";
+import concepts_art_literature_l9_p2_concepts from "./concepts/art/literature/l9/p2/concepts";
+import concepts_art_literature_l9_p3_concepts from "./concepts/art/literature/l9/p3/concepts";
+import concepts_art_literature_l9_p4_concepts from "./concepts/art/literature/l9/p4/concepts";
+import concepts_art_literature_l9_p5_concepts from "./concepts/art/literature/l9/p5/concepts";
+
+import concepts_art_literature_l10_p1_concepts from "./concepts/art/literature/l10/p1/concepts";
+import concepts_art_literature_l10_p2_concepts from "./concepts/art/literature/l10/p2/concepts";
+import concepts_art_literature_l10_p3_concepts from "./concepts/art/literature/l10/p3/concepts";
+import concepts_art_literature_l10_p4_concepts from "./concepts/art/literature/l10/p4/concepts";
+import concepts_art_literature_l10_p5_concepts from "./concepts/art/literature/l10/p5/concepts";
+
+
+
 
 export const CONCEPT_SETS: Record<string, { id: string; concepts: any[] }> = {
   art_painting_l1_p1: concepts_art_painting_l1_p1_concepts,
@@ -119,5 +181,65 @@ export const CONCEPT_SETS: Record<string, { id: string; concepts: any[] }> = {
   art_painting_l10_p3: concepts_art_painting_l10_p3_concepts,
   art_painting_l10_p4: concepts_art_painting_l10_p4_concepts,
   art_painting_l10_p5: concepts_art_painting_l10_p5_concepts,
+
+  art_literature_l1_p1: concepts_art_literature_l1_p1_concepts,
+  art_literature_l1_p2: concepts_art_literature_l1_p2_concepts,
+  art_literature_l1_p3: concepts_art_literature_l1_p3_concepts,
+  art_literature_l1_p4: concepts_art_literature_l1_p4_concepts,
+  art_literature_l1_p5: concepts_art_literature_l1_p5_concepts,
+
+  art_literature_l2_p1: concepts_art_literature_l2_p1_concepts,
+  art_literature_l2_p2: concepts_art_literature_l2_p2_concepts,
+  art_literature_l2_p3: concepts_art_literature_l2_p3_concepts,
+  art_literature_l2_p4: concepts_art_literature_l2_p4_concepts,
+  art_literature_l2_p5: concepts_art_literature_l2_p5_concepts,
+
+  art_literature_l3_p1: concepts_art_literature_l3_p1_concepts,
+  art_literature_l3_p2: concepts_art_literature_l3_p2_concepts,
+  art_literature_l3_p3: concepts_art_literature_l3_p3_concepts,
+  art_literature_l3_p4: concepts_art_literature_l3_p4_concepts,
+  art_literature_l3_p5: concepts_art_literature_l3_p5_concepts,
+
+  art_literature_l4_p1: concepts_art_literature_l4_p1_concepts,
+  art_literature_l4_p2: concepts_art_literature_l4_p2_concepts,
+  art_literature_l4_p3: concepts_art_literature_l4_p3_concepts,
+  art_literature_l4_p4: concepts_art_literature_l4_p4_concepts,
+  art_literature_l4_p5: concepts_art_literature_l4_p5_concepts,
+
+  art_literature_l5_p1: concepts_art_literature_l5_p1_concepts,
+  art_literature_l5_p2: concepts_art_literature_l5_p2_concepts,
+  art_literature_l5_p3: concepts_art_literature_l5_p3_concepts,
+  art_literature_l5_p4: concepts_art_literature_l5_p4_concepts,
+  art_literature_l5_p5: concepts_art_literature_l5_p5_concepts,
+
+  art_literature_l6_p1: concepts_art_literature_l6_p1_concepts,
+  art_literature_l6_p2: concepts_art_literature_l6_p2_concepts,
+  art_literature_l6_p3: concepts_art_literature_l6_p3_concepts,
+  art_literature_l6_p4: concepts_art_literature_l6_p4_concepts,
+  art_literature_l6_p5: concepts_art_literature_l6_p5_concepts,
+
+  art_literature_l7_p1: concepts_art_literature_l7_p1_concepts,
+  art_literature_l7_p2: concepts_art_literature_l7_p2_concepts,
+  art_literature_l7_p3: concepts_art_literature_l7_p3_concepts,
+  art_literature_l7_p4: concepts_art_literature_l7_p4_concepts,
+  art_literature_l7_p5: concepts_art_literature_l7_p5_concepts,
+
+  art_literature_l8_p1: concepts_art_literature_l8_p1_concepts,
+  art_literature_l8_p2: concepts_art_literature_l8_p2_concepts,
+  art_literature_l8_p3: concepts_art_literature_l8_p3_concepts,
+  art_literature_l8_p4: concepts_art_literature_l8_p4_concepts,
+  art_literature_l8_p5: concepts_art_literature_l8_p5_concepts,
+
+  art_literature_l9_p1: concepts_art_literature_l9_p1_concepts,
+  art_literature_l9_p2: concepts_art_literature_l9_p2_concepts,
+  art_literature_l9_p3: concepts_art_literature_l9_p3_concepts,
+  art_literature_l9_p4: concepts_art_literature_l9_p4_concepts,
+  art_literature_l9_p5: concepts_art_literature_l9_p5_concepts,
+
+  art_literature_l10_p1: concepts_art_literature_l10_p1_concepts,
+  art_literature_l10_p2: concepts_art_literature_l10_p2_concepts,
+  art_literature_l10_p3: concepts_art_literature_l10_p3_concepts,
+  art_literature_l10_p4: concepts_art_literature_l10_p4_concepts,
+  art_literature_l10_p5: concepts_art_literature_l10_p5_concepts,
  
 };
